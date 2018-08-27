81b0dd37412b2d12582ad71e414815af4e5ba2b9
TangentialDevelopment <joetang148@gmail.com>
2018-08-27 19:04:32 -0400

    finished the last 5 routes

---
+++ README.md
@@ -0,0 +1,4 @@
+### RESTful blog project
+### tools: semantic UI, express, ejs, mongodb, mongoose, body parser, method override, express-sanitizer
+
+description: A persistent blog that can upload and read information from a data base that covers all RESTful routes. Taken from Colt Steele web developers bootcamp
--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-var express    = require("express"),
-    app        = express(),
-    bodyParser = require("body-parser"),
-    mongoose   = require("mongoose");
+var expressSanitizer = require("express-sanitizer"),
+    express          = require("express"),
+    methodOverride   = require("method-override"),
+    app              = express(),
+    bodyParser       = require("body-parser"),
+    mongoose         = require("mongoose");
 
 mongoose.connect("mongodb://localhost:27017/restful_blog_app", { useNewUrlParser: true });
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+app.use(expressSanitizer());
 
 var blogSchema = new mongoose.Schema({
   title: String,
@@ -18,17 +22,18 @@ var blogSchema = new mongoose.Schema({
   }
 });
 var Blog = mongoose.model("Blog", blogSchema);
-//
+
 // Blog.create({
 //   title: "Test Blog",
-//   image: "https://images.unsplash.com/photo-1515408320194-59643816c5b2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=fcbebfe204ad7e04d558d7e0cbc0d2eb&auto=format&fit=crop&w=500&q=60",
-//   body: "hello this is a blog post"
-// })
+//   image: "http://res.cloudinary.com/simpleview/image/upload/v1469218578/clients/lanecounty/constitution_grove_campground_by_natalie_inouye_417476ef-05c3-464d-99bd-032bb0ee0bd5.png",
+//   body: "hello this is a blog post 2"
+// });
 
 app.get("/", function (req, res) {
   res.redirect("/blogs");
 });
 
+// index route
 app.get("/blogs", function(req, res) {
   Blog.find({}, function(err, blogs) {
     if (err) {
@@ -37,6 +42,68 @@ app.get("/blogs", function(req, res) {
         res.render("index", {blogs: blogs})
     }
   })
+});
+
+// new route
+app.get("/blogs/new", function(req, res) {
+  res.render("new");
+});
+
+// create route
+app.post("/blogs", function(req, res) {
+  req.body.blog.body = req.sanitize(req.body.blog.body);
+  Blog.create(req.body.blog, function(err, newBlog) {
+    if (err) {
+      res.render("new");
+    } else {
+      res.redirect("/blogs");
+    }
+  });
+});
+
+// show route
+app.get("/blogs/:id", function(req, res) {
+  Blog.findById(req.params.id, function(err, foundBlog) {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      res.render("show", {blog: foundBlog});
+    }
+  });
+});
+
+// edit route
+app.get("/blogs/:id/edit", function(req, res) {
+  Blog.findById(req.params.id, function(err, foundBlog) {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      res.render("edit", {blog: foundBlog});
+    }
+  });
+});
+
+// update route
+app.put("/blogs/:id", function(req, res) {
+  req.body.blog.body = req.sanitize(req.body.blog.body);
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      res.redirect("/blogs/" + req.params.id);
+    }
+  });
+});
+
+// delete route
+app.delete("/blogs/:id", function(req, res) {
+  Blog.findByIdAndRemove(req.params.id, function(err) {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      res.redirect("/blogs");
+    }
+  });
 });
 
 app.listen(3000, process.env.PORT, process.env.IP, function () {
